a39cdf26da63ba7a97cf5f70c398b73012fdb24b
rootate <https://github.com/umitaltintas/umitaltintas.com>
2021-03-11 20:12:15 +0300

    create anten class

---
+++ reply2021/src/Anten.java
@@ -0,0 +1,9 @@
+public class Anten {
+    
+    double latencyP;
+    double speedP;
+    Anten(double latencyP, double speedP){
+        this.latencyP = latencyP;
+        this.speedP = speedP;
+    }
+}
