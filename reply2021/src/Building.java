2a17bd8ce5b245f81b4cccb1df7af230979bac9a
rootate <https://github.com/umitaltintas/umitaltintas.com>
2021-03-11 20:27:02 +0300

    add coordinates to buildings

--- reply2021/src/Building.java
+++ reply2021/src/Building.java
@@ -1,12 +1,15 @@
 public class Building {
-    
+
     double latencyP;
     double speedP;
+    int coordinateX;
+    int coordinateY;
 
-
-    Building(double latencyP , double speedP){
+    Building(double latencyP, double speedP, int coordinateX, int coordinateY) {
         this.latencyP = latencyP;
         this.speedP = speedP;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
     }
 
 }
