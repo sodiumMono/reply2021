24167b8f2e646a25248c71a7b1418bf933a587a0
rootate <https://github.com/umitaltintas/umitaltintas.com>
2021-03-11 20:35:15 +0300

    complate reading datas

--- reply2021/src/App.java
+++ reply2021/src/App.java
@@ -1,5 +1,55 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        File file = new File("input.in");
+        int buildingCount = 0;
+        int antenaCount = 0;
+        int w;
+        int h;
+        double revard = 0;
+        Scanner scanner = new Scanner(file);
+
+        w = scanner.nextInt();
+        h = scanner.nextInt();
+        buildingCount = scanner.nextInt();
+        antenaCount = scanner.nextInt();
+        revard = scanner.nextInt();
+        ArrayList<Building> buildings = new ArrayList<>();
+        for (int i = 0; i < buildingCount; i++) {
+            int x;
+            int y;
+            int l;
+            int s;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            l = scanner.nextInt();
+            s = scanner.nextInt();
+            Building building = new Building(l, s, x, y);
+            buildings.add(building);
+
+        }
+        ArrayList<Anten> antens = new ArrayList<>();
+        for (int i = 0; i < buildingCount; i++) {
+            int r;
+
+            int s;
+
+            r = scanner.nextInt();
+            s = scanner.nextInt();
+            Anten anten = new Anten(r, s);
+            antens.add(anten);
+
+        }
+
+
+
+
+
+
+        
+        scanner.close();
     }
 }
